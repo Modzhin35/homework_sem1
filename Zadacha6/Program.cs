3735bc9988d33fac0974b993707fe89f55594fde
modzhin <modzhds@gmail.com>
2022-12-17 01:07:38 +0300

    Zadacha6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string answer;
+if (num % 2 == 0)
+{
+    answer = "четное";
+}
+else
+{
+    answer = "нечетное";
+}
+Console.Write($"Ваше число {num} {answer}");
