5f4dfef72c4ee9fdca1e1bf413b1d38363cb038c
modzhin <modzhds@gmail.com>
2022-12-17 00:45:47 +0300

    Zadacha4 correct

--- Zadacha4/Program.cs
+++ Zadacha4/Program.cs
@@ -5,10 +5,12 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
-if(num2>max){
-    max=num2;
+if (num2 > max)
+{
+    max = num2;
 }
-if(num3>max){
-    max=num3;
+if (num3 > max)
+{
+    max = num3;
 }
 Console.Write(max);
