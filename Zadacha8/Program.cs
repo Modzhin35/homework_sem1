02a24cda32f8a12a45af288dced35623fc8ba56d
modzhin <modzhds@gmail.com>
2022-12-17 01:25:02 +0300

    Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите любое число больше единицы: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 2)
+{
+    Console.Write("Число должно быть больше единицы!"); //В задаче условия вывести числа от 1 до N
+}
+int i = 2;
+while (i <= num)
+{
+    Console.Write($"{i} ");
+    i += 2;
+}
