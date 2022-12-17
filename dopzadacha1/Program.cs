c5faf81bdfad9c86473772ea552cf7048192d5f1
modzhin <modzhds@gmail.com>
2022-12-17 19:04:51 +0300

    dopzadacha2

---
+++ dopzadacha1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число, которое является квадратом первого, или не является. На ваш выбор: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int res = num1 * num1;
+if (num2 == res)
+{
+    Console.Write($"Число {num2} является квадратом числа {num1}");
+}
+else
+{
+    Console.Write($"Число {num2} не является квадратом числа {num1}");
+}
